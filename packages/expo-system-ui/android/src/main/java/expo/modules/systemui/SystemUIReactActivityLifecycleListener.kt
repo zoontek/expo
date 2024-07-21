545882423d6dbb1a2ee4212319c1ea76d30a923a
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-21 17:10:25 +0200

    Apply SystemUI.enableEdgeToEdge after setUserInterfaceStyle

--- packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIReactActivityLifecycleListener.kt
+++ packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIReactActivityLifecycleListener.kt
@@ -9,8 +9,8 @@ import expo.modules.systemui.singletons.SystemUI
 
 class SystemUIReactActivityLifecycleListener : ReactActivityLifecycleListener {
   override fun onCreate(activity: Activity, savedInstanceState: Bundle?) {
-    SystemUI.enableEdgeToEdge(activity)
     SystemUI.setUserInterfaceStyle(getUserInterfaceStyle(activity))
+    SystemUI.enableEdgeToEdge(activity)
   }
 
   private fun getUserInterfaceStyle(context: Context): String =
