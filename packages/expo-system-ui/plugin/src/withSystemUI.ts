ba68f26721afb2e3986451c68de0c731315d8669
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-25 20:12:14 +0200

    Add Theme.EdgeToEdge support

--- packages/expo-system-ui/plugin/src/withSystemUI.ts
+++ packages/expo-system-ui/plugin/src/withSystemUI.ts
@@ -1,5 +1,6 @@
 import { ConfigPlugin, createRunOncePlugin, withPlugins } from 'expo/config-plugins';
 
+import { withAndroidEdgeToEdgeTheme } from './withAndroidEdgeToEdgeTheme';
 import { withAndroidRootViewBackgroundColor } from './withAndroidRootViewBackgroundColor';
 import { withAndroidUserInterfaceStyle } from './withAndroidUserInterfaceStyle';
 import { withIosRootViewBackgroundColor } from './withIosRootViewBackgroundColor';
@@ -8,12 +9,21 @@ import { withIosUserInterfaceStyle } from './withIosUserInterfaceStyle';
 const pkg = require('expo-system-ui/package.json');
 
 const withSystemUI: ConfigPlugin = (config) => {
-  return withPlugins(config, [
+  const { experiments = {} } = config;
+  const { edgeToEdge = false } = experiments;
+
+  const plugins = [
     withAndroidRootViewBackgroundColor,
     withIosRootViewBackgroundColor,
     withAndroidUserInterfaceStyle,
     withIosUserInterfaceStyle,
-  ]);
+  ];
+
+  if (edgeToEdge) {
+    plugins.push(withAndroidEdgeToEdgeTheme);
+  }
+
+  return withPlugins(config, plugins);
 };
 
 export default createRunOncePlugin(withSystemUI, pkg.name, pkg.version);
