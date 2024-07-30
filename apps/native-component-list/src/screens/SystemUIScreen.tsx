d24fc79f9cd8a1a220acc71b61c8d0bf48c66901
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-30 16:57:59 +0200

    Add example screen

--- apps/native-component-list/src/screens/SystemUIScreen.tsx
+++ apps/native-component-list/src/screens/SystemUIScreen.tsx
@@ -1,6 +1,7 @@
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 import * as SystemUI from 'expo-system-ui';
 import * as React from 'react';
-import { ColorValue, ScrollView } from 'react-native';
+import { ColorValue, ScrollView, Switch, Text, View } from 'react-native';
 
 import Button from '../components/Button';
 import { Page, Section } from '../components/Page';
@@ -12,6 +13,9 @@ export default function SystemUIScreen() {
       <Page>
         <Section title="Background Color">
           <BackgroundColorExample />
+        </Section>
+        <Section title="System Bars">
+          <SystemBarsExample />
         </Section>
       </Page>
     </ScrollView>
@@ -35,6 +39,38 @@ function BackgroundColorExample() {
         onPress={async () => setColor(await SystemUI.getBackgroundColorAsync())}
         title={`Get background color: ${color?.toString()}`}
       />
+    </>
+  );
+}
+
+const STYLES_VALUES: SystemUI.SystemBarStyle[] = ['auto', 'light', 'dark'];
+
+function SystemBarsExample() {
+  const [hidden, setHidden] = React.useState(false);
+  const [style, setStyle] = React.useState(STYLES_VALUES[0]);
+
+  return (
+    <>
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text>Hidden</Text>
+        <Switch value={hidden} onValueChange={setHidden} />
+      </View>
+
+      <SegmentedControl
+        values={STYLES_VALUES}
+        selectedIndex={STYLES_VALUES.indexOf(style)}
+        onChange={(event) => {
+          setStyle(STYLES_VALUES[event.nativeEvent.selectedSegmentIndex]);
+        }}
+      />
+
+      <SystemUI.SystemBars hidden={hidden} style={style} />
     </>
   );
 }
